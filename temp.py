ac42f28e236c401572f4848ef468f2cdcc21d30b
Kaan Kasım Özdemir <76540235+kaanrone@users.noreply.github.com>
2020-12-24 20:41:43 +0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,47 @@
+#HW1
+
+value_1 = int(input('lutfen bir deger gir'))
+value_2 = int(input('lutfen bir deger gir'))
+value_3 = int(input('lutfen bir deger gir'))
+value_4 = int(input('lutfen bir deger gir'))
+value_5 = int(input('lutfen bir deger gir'))
+
+print("girdiginiz degerler: {} , {} , {} , {} , {} ".format(value_1,value_2,value_3,value_4,value_5))
+
+print(type(value_1))
+print(type(value_2))
+print(type(value_3))
+print(type(value_4))
+print(type(value_5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
